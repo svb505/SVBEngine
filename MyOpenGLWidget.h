8712732876fa2e82c8dd31570d2535e17a4ba9e4
svb <bulyginsvatoslav018@gmail.com>
2025-11-09 09:02:50 +0100

    Add projects files.

---
+++ MyOpenGLWidget.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <vector>
+#include "Objects.h"
+
+/*
+ * КЛАСС MyOpenGLWidget
+ * --------------------
+ * Это основное окно для отрисовки.
+ * Оно управляет контекстом OpenGL и списком объектов.
+ */
+class MyOpenGLWidget : public QOpenGLWidget, protected QOpenGLFunctions {
+    Q_OBJECT
+public:
+    explicit MyOpenGLWidget(QWidget* parent = nullptr);
+
+    void addObject(Object* obj); // добавить объект в сцену
+
+protected:
+    void initializeGL() override; // инициализация OpenGL
+    void resizeGL(int w, int h) override; // изменение размера окна
+    void paintGL() override; // отрисовка каждого кадра
+
+private:
+    std::vector<Object*> objects; // все объекты сцены
+};
